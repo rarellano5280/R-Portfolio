e8c3ff429b980b66a5c8b0e5908d50f9903e29bf
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-01-16 13:22:45 -0700

    added contact me section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import './contact.css'
+import React from 'react';
+import {useState} from 'react';
+import './contact.css';
 
-const Contact = () => {
+
+
+function Contact() {
+
+  const [message, setMessage] =useState(false)
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    setMessage(true);
+}
+
   return (
     <div className='port__contact section__padding'>
       <div className='port__contact-heading'>
        <h1 className='gradient__text'>Contact Me</h1>
+       <div />
       </div>
+      <div className='port__contact-form'>
+      <form onSubmit={handleSubmit} >
+        <input type="text" placeholder='Email' />
+        <input type="text" placeholder='Name' />
+        <textarea placeholder='message'></textarea>
+        <div className='port__contact-btn'>
+        <button type='submit'>Send</button>
+        {message && <span> Thank you. I will be in contact soon!</span>}
+        </div>
+        </form>
+      </div>
+
+
     </div>
   )
 }
-
 export default Contact
