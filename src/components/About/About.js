213f1c5fb5800f8d8ecac594001a3406b9b55d59
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-01-16 18:03:58 -0700

    added about me text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,10 @@ const About = () => {
       <div className='port__aboutme-heading'>
         <h1 className='gradient__text'>Let me introduce myself</h1>
         <div />
-        <p>Integer gravida at sapien ac gravida. Vestibulum non urna sed enim pellentesque lacinia vitae vel purus. Mauris quis ultrices dui, non auctor lectus. Phasellus auctor ligula libero, in vulputate augue rutrum a. Phasellus id sodales erat. Integer eget lorem ut velit mattis </p>
+        <p> Hello, thank you for visiting my portfolio! I'm a Software Developer who is eager to learn and grow in the space of tech. I recently received a certifcate in Full Stack Development from the University of Denver. 
+          I'm constatly building new apps to advance my knowledge in the ever changing world of software. I enjoy building projects that provide solutions to real-world problems.
+           When I'm not completely immersed in my computer, you can usually find me outside hiking with my Great Dane Oliver.  
+           </p>
       </div>
     </div>
 
